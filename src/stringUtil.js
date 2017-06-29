524a9cddf5f010fb016f0fb3dc0a10f6263d0165
Andres Gamboa <estebangam1@gmail.com>
2017-06-29 13:29:32 -0600

    adding nthWord

--- src/stringUtil.js
+++ src/stringUtil.js
@@ -4,4 +4,8 @@ stringUtil.firstWord = function(text) {
   return text.split(" ")[0];
 }
 
+stringUtil.nthWord = function(text, i) {
+  return text.split(" ")[i - 1];
+}
+
 module.exports = stringUtil;
