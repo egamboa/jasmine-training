ca949234cde2622e3ab8cf5301d8277b49270b7c
Andres Gamboa <estebangam1@gmail.com>
2017-06-29 13:28:51 -0600

    failing for nthWord

--- spec/stringUtilSpec.js
+++ spec/stringUtilSpec.js
@@ -1,9 +1,16 @@
 let stringUtil = require('../src/stringUtil');
 
-describe("stringUtil", function() {
-  describe("firstWord", function() {
+describe("stringUtil", function () {
+  describe("firstWord", function () {
     it("should return the first word of a string", function () {
       expect(stringUtil.firstWord("one two")).toBe("one");
+    });
+  });
+
+  describe("nthWord", function () {
+    it("should return the nth word of a string", function () {
+      expect(stringUtil.nthWord("one two", 1)).toBe("one");
+      expect(stringUtil.nthWord("one two", 2)).toBe("two");
     });
   });
 });
